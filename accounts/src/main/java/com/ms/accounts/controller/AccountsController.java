5db7325d0b4d18abd272fd4051b73674dea96745
amarmane64 <amarmane64@gmail.com>
2021-10-12 16:29:58 +0530

    pom files update and docker compose update

--- accounts/src/main/java/com/ms/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/ms/accounts/controller/AccountsController.java
@@ -4,6 +4,8 @@
 package com.ms.accounts.controller;
 
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +19,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.ms.accounts.config.AccountsServiceConfig;
 import com.ms.accounts.model.Accounts;
+import com.ms.accounts.model.Cards;
 import com.ms.accounts.model.Customer;
+import com.ms.accounts.model.CustomerDetails;
+import com.ms.accounts.model.Loans;
 import com.ms.accounts.model.Properties;
 import com.ms.accounts.repository.AccountsRepository;
+import com.ms.accounts.service.client.CardsFeignClient;
+import com.ms.accounts.service.client.LoansFeignClient;
 
 import io.micrometer.core.annotation.Timed;
 
@@ -33,8 +40,15 @@ public class AccountsController {
 	private AccountsRepository accountsRepository;
 	
 	@Autowired
-	AccountsServiceConfig accountsConfig;
-
+	private AccountsServiceConfig accountsConfig;
+	
+	
+	@Autowired
+	private CardsFeignClient cardsFeignClient;
+	
+	@Autowired
+	private LoansFeignClient loansFeignClient;
+	
 	@PostMapping("/myAccount")
 	@Timed(value = "getAccountDetails.time", description = "Time taken to return Account Details")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -57,5 +71,23 @@ public class AccountsController {
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
 	}
+	
+	
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		logger.info("myCustomerDetails() method started");
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardsDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+		logger.info("myCustomerDetails() method ended");
+		return customerDetails;
+
+	}
+
 
 }
